77b3cd0d661c4a3abf79e02ad7ee0e001c9ef3b9
Jake Braun <jakebraun14@gmail.com>
2023-09-20 14:46:41 -0500

    Basic fastapi setup

---
+++ FastAPI/main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI, HTTPException, Depends
+from typing import Annotated
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from database import SessionLocal, engine
+import models
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Set up middleware
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+)
+
+
+# Validate data format/type from front end
+class TransactionBase(BaseModel):
+    amount: float
+    category: str
+    description: str
+    is_income: bool
+    date: str
+
+
+class TransactionModel(TransactionBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+models.Base.metadata.create_all(bind=engine)
+
+
+@app.post("/transactions/", response_model=TransactionModel)
+async def create_transaction(transaction: TransactionBase, db: db_dependency):
+    db_transaction = models.Transaction(**transaction.model_dump())
+    db.add(db_transaction)
+    db.commit()
+    db.refresh(db_transaction)
+    return db_transaction
---
+++ FastAPI/models.py
@@ -0,0 +1,13 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean, Float
+
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    amount = Column(Float)
+    category = Column(String)
+    description = Column(String)
+    is_income = Column(Boolean)
+    date = Column(String)
