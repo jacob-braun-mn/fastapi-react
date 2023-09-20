4412b9ebd3e9bdf8fe161b40fc78ea9f8d1a051e
Jake Braun <jakebraun14@gmail.com>
2023-09-20 14:54:01 -0500

    Finished basic fastapi setup

--- FastAPI/database.py
+++ FastAPI/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = "sqlite://./finance.db"
+URL_DATABASE = "sqlite:///./finance.db"
 
 engine = create_engine(URL_DATABASE, connect_args={"check_same_thread": False})
 
--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends
-from typing import Annotated
+from typing import Annotated, List
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from database import SessionLocal, engine
@@ -53,3 +53,9 @@ async def create_transaction(transaction: TransactionBase, db: db_dependency):
     db.commit()
     db.refresh(db_transaction)
     return db_transaction
+
+
+@app.get("/transactions", response_model=List[TransactionModel])
+async def read_transactions(db: db_dependency, skip: int = 0, limit: int = 100):
+    transactions = db.query(models.Transaction).offset(skip).limit(limit).all()
+    return transactions
